9a7bacd53937ebd31ff4820e52c5cce2e6f7d280
Matt Tyler <me@matthewtyler.io>
2017-07-08 11:13:59 +0800

    Add command scaffolding

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "elasticsearch-operator",
+	Short: "An elasticsearch operator for Kubernetes",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+}
