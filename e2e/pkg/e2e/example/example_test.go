761a1363adf33a03a6cfb16d64f6c41b8aaa73cb
Matt Tyler <me@matthewtyler.io>
2017-07-18 18:48:33 +0800

    e2e: Add example tests
    Temporary example to help setting up e2e harness

---
+++ e2e/pkg/e2e/example/example_test.go
@@ -0,0 +1,32 @@
+package example_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Example", func() {
+	var (
+		a int
+		b int
+	)
+
+	BeforeEach(func() {
+		a = 5
+		b = 3
+	})
+
+	Describe("Law of mathematics applies in Australia", func() {
+		Context("When adding two numbers", func() {
+			It("should be equal to their sum", func() {
+				Expect(a + b).To(Equal(8))
+			})
+		})
+
+		Context("When multiplying two numbers", func() {
+			It("should be equal to their product", func() {
+				Expect(a * b).To(Equal(15))
+			})
+		})
+	})
+})
