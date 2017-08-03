265fe49384a6f7778a6d0fb0f3e69828ad40cdff
Matt Tyler <me@matthewtyler.io>
2017-08-03 20:06:53 +0800

    cmd: enable out-of-cluser usage

--- cmd/root.go
+++ cmd/root.go
@@ -3,14 +3,25 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/matt-tyler/elasticsearch-operator/pkg/client"
-	"github.com/matt-tyler/elasticsearch-operator/pkg/controller"
+	. "github.com/matt-tyler/elasticsearch-operator/pkg/client"
+	. "github.com/matt-tyler/elasticsearch-operator/pkg/controller"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"os/signal"
 	"syscall"
 )
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	return rest.InClusterConfig()
+}
 
 var RootCmd = &cobra.Command{
 	Use:   "elasticsearch-operator",
@@ -19,7 +30,9 @@ var RootCmd = &cobra.Command{
 		sigs := make(chan os.Signal, 1)
 		signal.Notify(sigs, syscall.SIGTERM)
 
-		clientConfig, err := rest.InClusterConfig()
+		kubeconfig := viper.GetString("kubeconfig")
+
+		clientConfig, err := buildConfig(kubeconfig)
 		if err != nil {
 			panic(err)
 		}
@@ -34,7 +47,7 @@ var RootCmd = &cobra.Command{
 			panic(err)
 		}
 
-		controller := controller.NewController(client, scheme)
+		controller := NewController(client, scheme)
 
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
@@ -55,5 +68,6 @@ func Execute() {
 }
 
 func init() {
-
+	RootCmd.PersistentFlags().StringP("kubeconfig", "f", "", "Path to kubeconfig")
+	viper.BindPFlag("kubeconfig", RootCmd.PersistentFlags().Lookup("kubeconfig"))
 }
